ef8674fb2ab9e41906ea8c64cfe903f21bc5b35a
CoreyHickson <corey.r.hickson@gmail.com>
2016-02-25 16:03:25 -0600

    Deleted stuff

--- indexScript.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$.get("https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=3&format=json",function(data,status){
-		$("#index_body").html(data[0]);
-	});
-});
