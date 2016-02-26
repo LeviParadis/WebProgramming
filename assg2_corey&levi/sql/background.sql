367f76096d34fa9118df502ead9d15610ae37a19
ldp343 <ldp343@mail.usask.ca>
2016-02-26 13:50:12 -0600

    my sql queries

---
+++ assg2_corey&levi/sql/background.sql
@@ -0,0 +1,13 @@
+INSERT INTO backgrounds
+    (background, proficiency)
+VALUES
+    ('Acolyte', 'Insight'),
+    ('Acolyte', 'Religion'),
+    ('Charlatan', 'Deception'),
+    ('Charlatan', 'Sleight of Hand'),
+    ('Criminal', 'Deception'),
+    ('Criminal', 'Stealth'),
+    ('Entertainer', 'Acrobatics'),
+    ('Entertainer', 'Performance'),
+    ('Hermit', 'Medicine'),
+    ('Hermit', 'Religion');
